aa4292f5d8b127ad3f86fec8262f90eb354e477a
Cesar Francisco Bassani da Luz <cluz@grupoimagetech.com.br>
2020-06-08 11:49:44 -0400

    Criacao do projeto contacts e estrutura de pastas para android e ios

---
+++ lib/android/android.app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AndroidApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(
+        child: Text(
+          "OK",
+        ),
+      ),
+    );
+  }
+}
