3ac94776eda9733a1f100f2b840b39c82cfa79bd
Cesar Francisco Bassani da Luz <cluz@grupoimagetech.com.br>
2020-06-08 16:26:37 -0400

    Imagem do contado na pagina details

--- lib/android/views/details.view.dart
+++ lib/android/views/details.view.dart
@@ -10,6 +10,34 @@ class DetailsView extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 10,
+            width: double.infinity,
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(200),
+            ),
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(100),
+                image: DecorationImage(
+                  image: NetworkImage("https://balta.io/imgs/andrebaltieri.jpg"),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
