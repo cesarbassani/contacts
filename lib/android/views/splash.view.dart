26c4e9026e6a3bbc747ac0ceeea8947977dc5f08
Cesar Francisco Bassani da Luz <cluz@grupoimagetech.com.br>
2020-06-08 14:24:34 -0400

    Centralizando os itens

--- lib/android/views/splash.view.dart
+++ lib/android/views/splash.view.dart
@@ -5,6 +5,17 @@ class SplashView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            width: double.infinity,
+          ),
+          Text("OK"),
+          Text("Teste"),
+        ],
+      ),
     );
   }
 }
