898755b2943b9ed4c8eaf3bf8fc1edc6d41215d7
Cesar Francisco Bassani da Luz <cluz@grupoimagetech.com.br>
2020-06-09 10:44:16 -0400

    Iniciando o App no iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 import 'package:contacts/android/android.app.dart';
 import 'package:flutter/material.dart';
+import 'package:contacts/ios/ios.app.dart';
 
 void main() {
   if(Platform.isIOS) {
-
+    runApp(IOSApp());
   } else {
     runApp(AndroidApp());
   }
